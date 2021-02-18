58b436380b27d71cd4653c708a3c5e53931202b1
alex inch <alexinch96@yahoo.com>
2021-02-18 07:20:10 +0100

    GMaps error handling
    Can now handle errors returned by the google maps API

--- src/App.js
+++ src/App.js
@@ -11,37 +11,40 @@ const App = () => {
   const lat = '-0.1278';
   let locationSearch = "";
   if (localStorage.getItem("Location")){
-    locationSearch = localStorage.getItem("Location");
+   locationSearch = localStorage.getItem("Location");
   }
   else {
-    locationSearch = "london, uk";
+    locationSearch = "london, uk"; // default fallback value if nothing is locally stored
   }
   
   const [currentInfo, setCurrent] = useState([]);
   const [currentDesc, setDesc] = useState([]);
   const [dailyTime, setDailyTime] = useState([]);
-  const [dailyDesc, setDailyDesc] = useState([]);
+  const [dailyDesc, setDailyDesc] = useState([]); 
   const [dailyMax, setDailyMax] = useState([]);
   const [dailyMin, setDailyMin] = useState([]);
   const [inputValue, setInputValue] = useState();
   const [searchValue, setSearchValue] = useState(locationSearch);
   const [locationDisplay, setLocationDisplay] = useState();
 
-  // triggered on page load, will be set up to refresh when new location is added
+  // Triggered on page load, will be set up to refresh when new location is added
   useEffect(() => {
     makeRequests(searchValue);
     localStorage.setItem("Location", searchValue);
   }, [searchValue]);
 
+  // Handles text submission and triggers useEffect, making the API calls
   const handleSubmit = (event) => {
     event.preventDefault();
     setSearchValue(inputValue);
   };
 
+  // Handles text input to the main search bar
   const handleChange = (event) => {
     setInputValue(event.target.value);
   }
 
+  // Returns the correct suffix for ordinal numbers (7th, 1st, 3rd)
   const nth = (d) => {
     if (d > 3 && d < 21){return d+'th'};
     switch (d % 10){
@@ -52,6 +55,7 @@ const App = () => {
     };
   };
 
+  // Converts javascript date strings to a more readable format
   const dateString = (dt) => {
     const dayArray = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'];
     const monthArray = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
@@ -63,16 +67,8 @@ const App = () => {
     return `${dayArray[dayIndex]} ${dateVal} ${monthArray[month]}`;
   }
 
-  // fetches and logs the data, updates the current info
-  const makeRequests = async (locationString) => {
-    const locReq = `https://maps.googleapis.com/maps/api/geocode/json?address=${locationString}&key=AIzaSyCMCT2J6HObmXkBZiD-gMAdmucOoTXEn_U`;
-    const locResponse = await fetch(locReq);
-    const locData = await locResponse.json();
-    const lat = locData.results[0].geometry.location.lat;
-    const lng = locData.results[0].geometry.location.lng;
-    setLocationDisplay(locData.results[0].formatted_address);
-    
-    // use longitude and latitude to build a 2nd API call to the weather API
+  // Fetches and handles openweathermap API call, updating hooks
+  const fetchWeather = async (lat, lng) => {
     const weatherReq = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lng}&units=metric&appid=a7ff38ee1c49b77064c72f94875dcc9e`;
     const weatherResponse = await fetch(weatherReq);
     const data = await weatherResponse.json();
@@ -83,6 +79,38 @@ const App = () => {
     setDailyMax( data.daily.map(x => Math.round(x.temp.max)) );
     setDailyMin( data.daily.map(x => Math.round(x.temp.min)) );
     console.log(data);
+  };
+
+  // Fetches and handles the GMaps API call, and calls fetchWeather upon successful return
+  const makeRequests = async (locationString) => {
+    const locReq = `https://maps.googleapis.com/maps/api/geocode/json?address=${locationString}&key=AIzaSyCMCT2J6HObmXkBZiD-gMAdmucOoTXEn_U`;
+    const locResponse = await fetch(locReq);
+    const locData = await locResponse.json();
+    let lat = "";
+    let lng = "";
+
+    switch(locData.status){
+      case "OK":
+        lat = locData.results[0].geometry.location.lat;
+        lng = locData.results[0].geometry.location.lng;
+        setLocationDisplay(locData.results[0].formatted_address);
+        fetchWeather(lat, lng);
+        break;
+      case "ZERO_RESULTS":
+        alert("No matching address found");
+        break;
+      case "INVALID_REQUEST":
+        alert("Invalid request");
+        break;
+      case "OVER_DAILY_LIMIT":
+      case "OVER_QUERY_LIMIT":
+      case "REQUEST_DENIED":
+      case "INVALID_REQUEST":
+        alert("API request failed");
+        break;
+      default:
+        alert("Unknown error occurred");
+    }
   };
 
   return(
